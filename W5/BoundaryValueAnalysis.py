0688dffa14ea4822f4af46ae1c607ac157ebc695
Trần Ngọc Hướng <82764704+huongtn1112@users.noreply.github.com>
2022-10-10 08:09:49 +0700

    add w5 testing function by tow method

---
+++ W5/BoundaryValueAnalysis.py
@@ -0,0 +1,13 @@
+import unittest
+import shop
+
+
+class BoundaryValueAnalysis(unittest.TestCase):
+
+    def test(self):
+        args = (50, 5000)
+        self.assertEqual(shop.get_product_reviews(*args), "a, 2")
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ W5/shop.py
@@ -0,0 +1,25 @@
+def get_product_reviews(price, ratio):
+    # 1: nhu cầu thấp, 2: nhu cầu trung bình, 3: nhu cầu cao
+    s = ""
+    try:
+        if ratio < 0 or ratio > 100 or price < 0 or price > 1000:
+            return "Invalid in"
+        else:
+            if 50 <= price <= 1000:
+                s += "a, "
+            else:
+                s += "b, "
+            if ratio <= 20:
+                s += "1"
+            elif ratio <= 70:
+                s += "2"
+            else:
+                s += "3"
+
+    except:
+        return "Invalid in"
+    return s
+
+
+if __name__ == '__main__':
+    print(get_product_reviews(0, 50))
