3be2cb29e0ae1ad66e5cd1d17fd0a804fc3c2db6
Trần Ngọc Hướng <82764704+huongtn1112@users.noreply.github.com>
2022-10-10 16:29:08 +0700

    Update BoundaryValueAnalysis.py

--- W5/BoundaryValueAnalysis.py
+++ W5/BoundaryValueAnalysis.py
@@ -4,9 +4,41 @@ import shop
 
 class BoundaryValueAnalysis(unittest.TestCase):
 
-    def test(self):
-        args = (50, 5000)
+    def test1(self):
+        args = (0, 50)
+        self.assertEqual(shop.get_product_reviews(*args), "b, 2")
+
+    def test2(self):
+        args = (1, 50)
+        self.assertEqual(shop.get_product_reviews(*args), "b, 2")
+
+    def test3(self):
+        args = (1000, 50)
         self.assertEqual(shop.get_product_reviews(*args), "a, 2")
+
+    def test4(self):
+        args = (999, 50)
+        self.assertEqual(shop.get_product_reviews(*args), "a, 2")
+
+    def test5(self):
+        args = (500, 50)
+        self.assertEqual(shop.get_product_reviews(*args), "a, 2")
+
+    def test6(self):
+        args = (500, 0)
+        self.assertEqual(shop.get_product_reviews(*args), "a, 1")
+
+    def test7(self):
+        args = (500, 1)
+        self.assertEqual(shop.get_product_reviews(*args), "a, 1")
+
+    def test8(self):
+        args = (500, 100)
+        self.assertEqual(shop.get_product_reviews(*args), "a, 3")
+
+    def test9(self):
+        args = (500, 99)
+        self.assertEqual(shop.get_product_reviews(*args), "a, 3")
 
 
 if __name__ == '__main__':
