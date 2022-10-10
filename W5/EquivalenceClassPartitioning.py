900f7b36953fa1ed7952d063b31c59c5d8dfd85a
Trần Ngọc Hướng <82764704+huongtn1112@users.noreply.github.com>
2022-10-10 16:28:46 +0700

    Update EquivalenceClassPartitioning.py

--- W5/EquivalenceClassPartitioning.py
+++ W5/EquivalenceClassPartitioning.py
@@ -4,9 +4,41 @@ import shop
 
 class EquivalenceClassPartitioning(unittest.TestCase):
 
-    def test(self):
-        args = (50, 5000)
-        self.assertEqual(shop.get_product_reviews(*args), "a, 2")
+    def test1(self):
+        args = (-10, 10)
+        self.assertEqual(shop.get_product_reviews(*args), "Invalid in")
+
+    def test2(self):
+        args = (20, 10)
+        self.assertEqual(shop.get_product_reviews(*args), "b, 1")
+
+    def test3(self):
+        args = (70, 10)
+        self.assertEqual(shop.get_product_reviews(*args), "a, 1")
+
+    def test4(self):
+        args = (2000, 10)
+        self.assertEqual(shop.get_product_reviews(*args), "Invalid in")
+
+    def test5(self):
+        args = (10, -10)
+        self.assertEqual(shop.get_product_reviews(*args), "Invalid in")
+
+    def test6(self):
+        args = (10, 10)
+        self.assertEqual(shop.get_product_reviews(*args), "b, 1")
+
+    def test7(self):
+        args = (10, 50)
+        self.assertEqual(shop.get_product_reviews(*args), "b, 2")
+
+    def test8(self):
+        args = (10, 75)
+        self.assertEqual(shop.get_product_reviews(*args), "b, 3")
+
+    def test9(self):
+        args = (10, 110)
+        self.assertEqual(shop.get_product_reviews(*args), "Invalid in")
 
 
 if __name__ == '__main__':
